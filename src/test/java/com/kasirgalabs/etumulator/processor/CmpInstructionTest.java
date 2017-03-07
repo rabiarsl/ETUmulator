af3f72f268e284f09d02d73aa67c9b0ef71b917b
rootg <gorkemmulayim@icloud.com>
2017-03-07 18:07:01 +0300

    Addec cmn and cmp tests.

---
+++ src/test/java/com/kasirgalabs/etumulator/processor/CmpInstructionTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2017 Kasirgalabs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.kasirgalabs.etumulator.processor;
+
+import static org.junit.Assert.assertEquals;
+
+import com.kasirgalabs.etumulator.InstructionTester;
+import org.junit.Test;
+
+public class CmpInstructionTest extends InstructionTester {
+    /**
+     * Test of exitCmp method, of class Processor.
+     */
+    @Test
+    public void testExitCmp() {
+        char[] code = ("cmp r2, #0\n").toCharArray();
+        runTestCode(code);
+        assertEquals("Negative flag is wrong.", false, cpsr.isNegative());
+        assertEquals("Zero flag is wrong.", true, cpsr.isZero());
+        assertEquals("Overflow flag is wrong.", false, cpsr.isOverflow());
+
+        code = ("cmp r2, 8\n").toCharArray();
+        runTestCode(code);
+        assertEquals("Negative flag is wrong.", true, cpsr.isNegative());
+        assertEquals("Zero flag is wrong.", false, cpsr.isZero());
+        assertEquals("Overflow flag is wrong.", false, cpsr.isOverflow());
+
+        code = ("mov r0, #0xf0\n"
+                + "cmp r0, 0xf0\n").toCharArray();
+        runTestCode(code);
+        assertEquals("Negative flag is wrong.", false, cpsr.isNegative());
+        assertEquals("Zero flag is wrong.", true, cpsr.isZero());
+        assertEquals("Overflow flag is wrong.", false, cpsr.isOverflow());
+
+        code = ("ldr r1, =#0x80000000\n"
+                + "ldr r2, =0xffffffff\n"
+                + "cmp r1, r2\n").toCharArray();
+        runTestCode(code);
+        assertEquals("Negative flag is wrong.", true, cpsr.isNegative());
+        assertEquals("Zero flag is wrong.", false, cpsr.isZero());
+        assertEquals("Overflow flag is wrong.", false, cpsr.isOverflow());
+    }
+}
