1634ea2853ace6ff7c2fdfcb0c8f92d20add6d84
rootg <gorkemmulayim@icloud.com>
2017-03-07 11:52:46 +0300

    Added orn and orns tests.

---
+++ src/test/java/com/kasirgalabs/etumulator/processor/OrnsInstructionTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2017 Kasirgalabs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.kasirgalabs.etumulator.processor;
+
+import static org.junit.Assert.assertEquals;
+
+import com.kasirgalabs.etumulator.InstructionTester;
+import org.junit.Test;
+
+public class OrnsInstructionTest extends InstructionTester {
+    /**
+     * Test of exitOrns method, of class Processor.
+     */
+    @Test
+    public void testExitOrns() {
+        char[] code = ("mov r1, #0\n"
+                + "mov r2, #1\n"
+                + "orns r0, r1, r2\n").toCharArray();
+        runTestCode(code, true);
+        assertEquals("OR result is wrong.", registerFile.getValue(0), -2);
+        assertEquals("Negative flag is wrong.", true, cpsr.isNegative());
+        assertEquals("Zero flag is wrong.", false, cpsr.isZero());
+
+        code = ("ldr r1, =0xffffffff\n"
+                + "orns r0, r1, r1\n").toCharArray();
+        runTestCode(code, true);
+        assertEquals("OR result is wrong.", registerFile.getValue(0), 0xffffffff | ~0xffffffff);
+        assertEquals("Negative flag is wrong.", true, cpsr.isNegative());
+        assertEquals("Zero flag is wrong.", false, cpsr.isZero());
+
+        code = ("ldr r1, =0xffffffff\n"
+                + "orns r0, r1, 0\n").toCharArray();
+        runTestCode(code, true);
+        assertEquals("OR result is wrong.", registerFile.getValue(0), -1);
+        assertEquals("Negative flag is wrong.", true, cpsr.isNegative());
+        assertEquals("Zero flag is wrong.", false, cpsr.isZero());
+
+        code = ("orns r0, r1, 0xffffffff\n").toCharArray();
+        runTestCode(code, true);
+        assertEquals("OR result is wrong.", registerFile.getValue(0), 0);
+        assertEquals("Negative flag is wrong.", false, cpsr.isNegative());
+        assertEquals("Zero flag is wrong.", true, cpsr.isZero());
+    }
+}
